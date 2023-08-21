db15f50874c767675d10799d433e288053665267
Samiksha <120582351+samiksha-pattir@users.noreply.github.com>
2023-08-21 11:35:16 +0530

    Add files via upload

---
+++ color_box.js
@@ -0,0 +1,42 @@
+let color_in =document.getElementById("box")
+
+
+let one = document.getElementById("yellow")
+one.onclick = function(){
+    color_in.style.backgroundColor ="yellow" ;
+    
+    
+}
+
+let two = document.getElementById("red")
+two.onclick = function(){
+    box.style.backgroundColor ="red" ;
+    
+}
+
+let three = document.getElementById("blue")
+three.onclick = function(){
+    box.style.backgroundColor ="blue" ;
+    
+}
+
+
+
+let four = document.getElementById("green")
+four.onclick = function(){
+    box.style.backgroundColor ="green" ;
+    
+}
+
+
+
+
+let paragraph =document.getElementById("text")
+
+let change = document.getElementById("click")
+change.onclick =function(){
+    paragraph.innerHTML ="The Gurukul for the New Age India and its New Generation  We are a non-profit dedicated to bring affordable tech education to underprivileged girls in India.";
+    paragraph.style.color ="blue";
+}
+
+
